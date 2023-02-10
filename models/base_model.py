4f2e52285a8844ddbb68c1982256dc0a629d1dc2
kadimakeith <keithkadima@gmail.com>
2023-02-10 15:36:53 +0300

    Complete base_model class: Task 4

--- models/base_model.py
+++ models/base_model.py
@@ -41,6 +41,7 @@ class BaseModel:
         rdict["created_at"] = self.created_at.isoformat()
         rdict["updated_at"] = self.updated_at.isoformat()
         rdict["__class__"] = self.__class__.__name__
+        
         return rdict
 
     def __str__(self):
